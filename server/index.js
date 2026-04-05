e602feb0d3c1a8b92c4ee1b475b6500ac7fcd975
Mayank Roy <mayankroy106106@gmail.com>
2026-04-05 19:34:54 +0530

    .env and CORS changes

--- server/index.js
+++ server/index.js
@@ -5,7 +5,15 @@ import { supabase } from "./lib/supabase.js";
 import { generateTags, generateEmbedding } from "./lib/ai.js";
 
 const app = express();
-app.use(cors({ origin: ["http://localhost:5173", /^chrome-extension:\/\//] })); // Vite default port
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://memex-beta.vercel.app/",
+      /^chrome-extension:\/\//,
+    ],
+  }),
+); // Vite default port
 app.use(express.json());
 
 // Test route
